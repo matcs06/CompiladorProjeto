5ad39ef36ec59a149e950cce8781b7c0b4346d69
Mateus Costa <matcs06@gmail.com>
2019-09-29 00:13:19 -0300

    Adcionando a pizza

--- removeComment.js
+++ removeComment.js
@@ -1,5 +1,12 @@
 exports.initRemov = function(arr1, arr2) {
   const contador = 0;
 
-  for (i = 0; i < arr2.length; i++) {}
+  for (i = 0; i < arr2.length; i++) {
+    if (arr2[i] == "/" && arr2[i + 1] == "/") {
+      contador++;
+    }
+
+    if (contador == 1) {
+    }
+  }
 };
